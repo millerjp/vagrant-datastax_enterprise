48e4f5f1f0c539b036d17a6f59642b4bd66496a2
Johnny Miller <johnny@jpm-macbook.local>
2015-01-06 12:20:38 +0100

    new set-up

---
+++ 3_DSE4_5_1/config/dse.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo ">>> Starting Install DataStax Enterprise 4.5.1 Script"
+sudo apt-get install dse-full=4.5.1-1 dse=4.5.1-1 dse-hive=4.5.1-1 dse-pig=4.5.1-1 dse-demos=4.5.1-1 dse-libsolr=4.5.1-1 dse-libtomcat=4.5.1-1 dse-libsqoop=4.5.1-1 dse-liblog4j=4.5.1-1 dse-libmahout=4.5.1-1 dse-libhadoop-native=4.5.1-1 dse-libcassandra=4.5.1-1 dse-libhive=4.5.1-1 dse-libpig=4.5.1-1 dse-libhadoop=4.5.1-1 dse-libspark=4.5.1-1 -y
